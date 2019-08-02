391ae12e98c03b2083c86c518f4e82849f4877e4
Paul Beckwith <303dog@gmail.com>
2019-08-02 19:06:30 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,14 +10,13 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {:name =>"Lord Capulet",:age => 50
+      :patriarch => {:name =>"Lord Capulet",:age => "50"
         },
-      :matriarch => {:name =>"Lady Capulet",:age => 51
+      :matriarch => {:name =>"Lady Capulet",:age => "51"
         },
-      :heroine => {
-        #your key/value pairs here
+      :heroine => {:name =>"Juliet",:age => "15",:status =>"alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [{name:"Benvolio", age: "17",attitude: "worried"}{name: "Mercutio", age: "18", attitude: "hot-headed"}]
    }
   }
 
