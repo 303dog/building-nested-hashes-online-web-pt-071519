374b3090bf9dcf74595fde0ab044c791269ba9da
Paul Beckwith <303dog@gmail.com>
2019-08-02 18:45:14 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,12 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriach =>{},:matriarch =>{},:hero =>{},:hero_friends =>[]}, 
+    :montague => {
+      :patriach =>{},
+      :matriarch =>{},
+      :hero =>{},
+      :hero_friends =>[]
+      
+    }, 
     :capulet => {:patriach =>{},:matriarch =>{},:heroine =>{},:heroine_friends =>[]
       
     }
